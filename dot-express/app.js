7d290281818500f5c307e841983ca610a258cbe4
END98 <takayoshi-endo@spicebox.co.jp>
2017-11-09 21:26:20 +0900

    ADD: custom middleware

--- dot-express/app.js
+++ dot-express/app.js
@@ -2,11 +2,17 @@ var express = require('express'),
     app = express();
 
     // Middleware: sequentialにMiddlewareの処理が実行される
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/ublic'));
+
+    // custom Middleware
+    app.use(function(req,res,next){
+        console.log('my custom middleware');
+        next();
+    });
 
     app.get('/hello.txt', function(req, res){
-    res.sendfile(__dirname + '/public/hello.txt');
-});
+        res.sendfile(__dirname + '/public/hello.txt');
+    });
 
 // 3,000のportで受け付ける
 app.listen(3000);
