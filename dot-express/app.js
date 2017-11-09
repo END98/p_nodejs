e5c40828b145e1121626d5e7833c128cd684ee02
END98 <takayoshi-endo@spicebox.co.jp>
2017-11-09 20:31:52 +0900

    ADD: app.use(express.static...) で静的なファイルへのアクセス処理をする

--- dot-express/app.js
+++ dot-express/app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express();
-
+    //静的なファイルのアクセスはここで処理をする
+    app.use(express.static(__dirname + '/public'));
 app.get('/hello.txt', function(req, res){
     res.sendfile(__dirname + '/public/hello.txt');
 });
