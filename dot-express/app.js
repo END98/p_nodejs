3e0f1f8ead7e11ce86e7dc3ebe0b6c0447adf792
END98 <takayoshi-endo@spicebox.co.jp>
2017-11-09 20:00:25 +0900

    ADD: app.get('/about')

--- dot-express/app.js
+++ dot-express/app.js
@@ -1,11 +1,15 @@
 var express = require('express'),
     app = express();
-    
+
 // '/'でアクセスしたら,function実行
 app.get('/',function(req,res){
     res.send('hello world');
 });
 
+app.get('/about',function(req,res){
+    res.send('about this page!');
+});
+
 // 3,000のportで受け付ける
-app.listent(3000);
+app.listen(3000);
 console.log("server starting...")
