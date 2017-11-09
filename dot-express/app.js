6f4ce9a42a7f1417d392f53fb8955c05b88b6b66
END98 <takayoshi-endo@spicebox.co.jp>
2017-11-09 20:28:16 +0900

    ADD: URLからファイルを読み出す

--- dot-express/app.js
+++ dot-express/app.js
@@ -1,29 +1,9 @@
 var express = require('express'),
     app = express();
 
-// オプショナル
-app.get('/users/:name?',function(req, res){
-    if(req.params.name){
-        res.send('hello,' + req.params.name);
-    } else {
-        res.send('hello nobady!');
-    }
+app.get('/hello.txt', function(req, res){
+    res.sendfile(__dirname + '/public/hello.txt');
 });
-
-//parameterに正規表現をかける
-app.get('/items/:id([0-9]+)', function(req,res){
-    res.send('itme no: ' + req.params.id);
-});
-
-/*
-app.get('/',function(req,res){
-    res.send('hello world');
-});
-
-app.get('/about',function(req,res){
-    res.send('about this page!');
-});
-*/
 
 // 3,000のportで受け付ける
 app.listen(3000);
