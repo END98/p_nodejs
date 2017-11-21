546fa28bd925a68f83c7db7d4def309e2ff1c234
END98 <takayoshi-endo@spicebox.co.jp>
2017-11-21 18:52:21 +0900

    ADD: Postされたデータを取得する方法

--- dot-express/app.js
+++ dot-express/app.js
@@ -1,36 +1,24 @@
 var express = require('express'),
     app = express();
     
-    //Edit log
     var logger = require('morgan');
-
-    //templete file 'EJS'
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
+    
+    //Post
+    app.use(express.json());
+    app.use(express.urlencoded());
 
     app.use(logger('dev'));
     app.use(express.static(__dirname + '/public'));
 
-    /*
-    app.get('/', function(req, res){
-        res.render('index', {title: 'title'});
-    });
-    */
-
-    app.param('id', function(req, res, next, id){
-        var users = ['taguchi', 'fkoji', 'dotinstall'];
-        req.params.name = users[id];
-        next();
+    app.get('/new', function(req, res){
+        res.render('new');
     });
 
-    app.get('/hello/:id', function(req, res){
-        res.send('hello ' + req.params.name);
+    app.post('/create', function(req, res){
+        res.send(req.body.name);
     });
-
-    app.get('/bye/:id', function(req, res){
-        res.send('bye ' + req.params.name);
-    });
-
 
 // 3,000のportで受け付ける
 app.listen(3000);
