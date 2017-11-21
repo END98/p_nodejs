9301be85db7aadacaf48a7b07c624613c4e80362
END98 <takayoshi-endo@spicebox.co.jp>
2017-11-21 18:41:43 +0900

    ADD: app.param ルーティングの処理の中ででてきたパラメータに対して共通処理をする

--- dot-express/app.js
+++ dot-express/app.js
@@ -11,9 +11,26 @@ var express = require('express'),
     app.use(logger('dev'));
     app.use(express.static(__dirname + '/public'));
 
+    /*
     app.get('/', function(req, res){
         res.render('index', {title: 'title'});
     });
+    */
+
+    app.param('id', function(req, res, next, id){
+        var users = ['taguchi', 'fkoji', 'dotinstall'];
+        req.params.name = users[id];
+        next();
+    });
+
+    app.get('/hello/:id', function(req, res){
+        res.send('hello ' + req.params.name);
+    });
+
+    app.get('/bye/:id', function(req, res){
+        res.send('bye ' + req.params.name);
+    });
+
 
 // 3,000のportで受け付ける
 app.listen(3000);
